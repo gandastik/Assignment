9c31fb08e656d1cba2291db15239a39d4a2d78c5
gandastik <gandastik@gmail.com>
2020-11-27 14:41:02 +0700

    add week16

---
+++ week16/secret_code.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+/*
+63010035
+for Profund weekly assignment at week 16 (27 Nov. 2020)
+*/
+int stringLength(char* str)
+{
+    int counter = 0;
+    while(*str != '\0')
+    {
+        counter++;
+        str++;
+    }
+    return counter;
+}
+
+int main()
+{
+    char str[100];
+    scanf("%[^\n]", &str);
+    char *ptr = str;
+    for(int i = 0; i<stringLength(ptr) ;i++)
+    {
+        if(str[i] >= 48 && str[i] <= 57)
+        {
+            printf("%c", str[i]);
+        }
+        if(str[i] >= 65 && str[i] <= 90)
+        {
+            printf("%c", str[i]);
+        }
+        if(str[i] >= 97 && str[i] <= 122)
+        {
+            printf("%c", str[i]);
+        }
+    }
+
+    return 0;
+}
+
